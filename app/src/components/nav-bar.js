2855eb992d0d0daabc371c7e266ec54bcd3e433f
Neitan2001 <natanestudante2001@hotmail.com>
2021-06-28 23:35:55 -0300

    Add navbar

---
+++ app/src/components/nav-bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/nav-bar.css';
+
+const NavBar = () => {
+    return (
+        <div className="navbar-container">
+            
+        </div>
+    );
+}
+
+export default NavBar;
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -4,11 +4,28 @@ import SearchBar from '../components/search-bar';
 import DiscoverNewBook from '../components/discover-new-book';
 import CurrentReading from '../components/current-reading';
 import greetinIcon from '../assets/greeting-icon.svg';
+import NavBar from '../components/nav-bar';
+
+
 
 const Home = () => {
-    return(
+
+    const reviewVideos = [
+        {
+            name: "Don't Make Me Think",
+            src: "https://i.ytimg.com/vi/vBzBgewl4ac/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAtbXNCBM81reFzf6jGqBWScJICwQ"
+        },
+        {
+            name: "Porém Bruxa, Carol Chiovatto",
+            src: "https://i.ytimg.com/vi/edUeMCa_Zf4/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLCtmW0eTDr5zet4ZyqgIw01ZrByeQ"
+        }
+    ]
+
+    return (
         <div className="home-container">
-            <SearchBar />
+            <div className="search-bar-container">
+                <SearchBar />
+            </div>
             <div className="greeting-container">
                 <h1>Olá, </h1>
                 <h1 className="user-name">Natan Tavares</h1>
@@ -20,9 +37,23 @@ const Home = () => {
             <div className="current-div">
                 <CurrentReading />
             </div>
-            <div className="reviews">
-                <h2>Reviews of The Days</h2>
-                <p>All Video</p>
+            <div className="reviews-container">
+                <div className="reviews-titles">
+                    <h2>Reviews of The Days</h2>
+                    <p>All Video</p>
+                </div>
+                {
+                    reviewVideos.map((videos) => {
+                        return (
+                            <div className="videos-container">
+                                <img src={videos.src} alt={videos.name} />
+                            </div>
+                        );
+                    })
+                }
+            </div>
+            <div className="nav-bar-div">
+                <NavBar/>
             </div>
         </div>
     );
@@ -30,4 +61,3 @@ const Home = () => {
 
 export default Home;
 
-  
